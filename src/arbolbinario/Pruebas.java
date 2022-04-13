56aa59492e20a08accd77cbd93e0bb3e88e51d28
F1r3F0x21 <fransanchezdelaparra@gmail.com>
2022-04-13 12:38:50 +0200

    master

--- src/arbolbinario/Pruebas.java
+++ src/arbolbinario/Pruebas.java
@@ -5,7 +5,6 @@ public class Pruebas {
     public static void main(String[] args) {
         System.out.println("********** PRUEBAS ARBOL BINARIO DE EXPRESION **********");
 
-
         String expresion1 = "52+83-*4/";
         //String expresion2 = "92+3+4*";
         //String expresion3 = "29*37-5*+8/";
