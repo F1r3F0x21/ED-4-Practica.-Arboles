498a1afafd1f7ae8b07fc12c6fdc9387992a702f
F1r3F0x21 <fransanchezdelaparra@gmail.com>
2022-04-25 13:24:15 +0200

    master

--- src/arbolbinario/Pruebas.java
+++ src/arbolbinario/Pruebas.java
@@ -9,39 +9,29 @@ public class Pruebas {
         String expresion3 = "29*37-5*+8/";
         String expresion4 = "9";
 
-<<<<<<< HEAD
-        Arbol ex = new Arbol(expresion1);
-=======
-        Arbol ex1 = new Arbol(expresion1);      
->>>>>>> d0b46618c5d2fddd5b983746b90744f791a1689c
+        Arbol ex1 = new Arbol(expresion1);
         Arbol ex2 = new Arbol(expresion2);
         Arbol ex3 = new Arbol(expresion3);
         Arbol ex4 = new Arbol(expresion4);
 
-<<<<<<< HEAD
-        ex.MostrarExpresion();
-=======
         ex1.MostrarExpresion();
         System.out.println();
         System.out.println("Resultado: " + ex1.calcularValor());
->>>>>>> d0b46618c5d2fddd5b983746b90744f791a1689c
+        System.out.println();
+
         ex2.MostrarExpresion();
         System.out.println();
         System.out.println("Resultado: " + ex2.calcularValor());
+        System.out.println();
+
         ex3.MostrarExpresion();
         System.out.println();
         System.out.println("Resultado: " + ex3.calcularValor());
+        System.out.println();
+
         ex4.MostrarExpresion();
-<<<<<<< HEAD
-
-       System.out.println(ex.calcularValor());
-       System.out.println(ex2.calcularValor());
-       System.out.println(ex3.calcularValor());
-       System.out.println(ex4.calcularValor());
-
-=======
         System.out.println();
         System.out.println("Resultado: " + ex4.calcularValor());
->>>>>>> d0b46618c5d2fddd5b983746b90744f791a1689c
+        System.out.println();
     }
 }
